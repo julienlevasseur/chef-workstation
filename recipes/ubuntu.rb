168083b349d71cf5c90de6ee374f8d6e868f851d
Julien Levasseur <levasju1@LAP-MTL-LEVASJU.vasco.com>
2018-01-24 14:26:39 -0500

    remove the display_manager cookbook_file resource

--- recipes/ubuntu.rb
+++ recipes/ubuntu.rb
@@ -3,10 +3,3 @@
 # Recipe:: ubuntu
 #
 # Copyright:: 2017, Julien Levasseur, All Rights Reserved.
-
-cookbook_file '/opt/display_manager.py' do
-  source 'display_manager.py'
-  owner 'root'
-  group 'root'
-  mode '0755'
-end
